75b86acc773adb8694cad1bd04100fd26ba74486
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-10-01 14:20:21 -0500

    add umami

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,6 +27,13 @@ const config = {
     defaultLocale: "en",
     locales: ["en"],
   },
+  scripts: [
+    {
+      src: 'https://umami.ersatztv.org/script.js',
+      defer: true,
+      'data-website-id': 'a4c678e2-348a-426f-b9b5-4a93ceba6b36'
+    }
+  ],
   presets: [
     [
       "classic",
